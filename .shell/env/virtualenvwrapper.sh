8c4f66ce15fe88dec609b9940a398d375004a6c4
Tony Narlock <tony@git-pull.com>
2015-03-22 16:36:11 -0700

    Whew: get virtualenvwrapper working with pyenv-virtualevwrapper

--- .shell/env/virtualenvwrapper.sh
+++ .shell/env/virtualenvwrapper.sh
@@ -1,6 +1,10 @@
-#!/bin/zsh
+#!/bin/sh
 
-if [ -f $HOME/.local/bin/virtualenvwrapper.sh ]; then
+# https://github.com/yyuu/pyenv-virtualenvwrapper/issues/19#issuecomment-54558646
+# zprezto python module
+if command -v pyenv > /dev/null 2>&1 && pyenv commands | grep -q "virtualenvwrapper"; then
+    pyenv virtualenvwrapper_lazy
+elif [ -f $HOME/.local/bin/virtualenvwrapper.sh ]; then
     . $HOME/.local/bin/virtualenvwrapper.sh
 elif [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
     . /usr/local/bin/virtualenvwrapper.sh
