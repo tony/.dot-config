51f1b50b5dabd67b4941d25adec88f3f73f20966
Tony Narlock <tony@git-pull.com>
2015-04-05 13:08:08 -0500

    shell alias for configuring vim

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 alias clear_pyc='find . -name "*.pyc" -exec rm -rf {} \;'
+
+alias configure_vim='./configure --with-features=huge  --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-luainterp'
