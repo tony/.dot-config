54afe259153c00da9652f3d10e09b1a56af3079c
Tony Narlock <tony@git-pull.com>
2015-05-05 14:10:01 -0500

    configure_vim alias: add --enable-gui so we get +xterm_clipboard

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -2,4 +2,4 @@
 
 alias clear_pyc='find . -name "*.pyc" -exec rm -rf {} \;'
 
-alias configure_vim='./configure --with-features=huge  --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-luainterp --with-luajit'
+alias configure_vim='./configure --with-features=huge --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-luainterp --enable-gui --enable-multibyte'
