64ebe337a405bd6cc31583a059df2d3f169aab3a
Tony Narlock <tony@git-pull.com>
2015-07-01 02:06:06 -0500

    export BASE16_SCHEME

--- .shell/env.d/base16-shell.sh
+++ .shell/env.d/base16-shell.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-BASE16_SCHEME="monokai"
+export BASE16_SCHEME="solarized"
 BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
 [[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
