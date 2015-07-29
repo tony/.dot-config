7e5dd06c14a94fc9c1f926b7e541a84d3125ef58
Tony Narlock <tony@git-pull.com>
2015-07-29 04:20:43 -0500

    update dual monitor

--- .screenlayout/dual-monitor.sh
+++ .screenlayout/dual-monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP3 --off --output DP2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 2560x672 --rotate normal --output VGA1 --off
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 3840x0 --rotate normal --output VGA1 --off
