d26eb84b7c5b8c7b5dcac4fc84116b0120c20c7d
Tony Narlock <tony@git-pull.com>
2015-09-22 13:08:59 -0500

    npm.sh paths were adding a lot more than we wanted, also add .node/bin

--- .shell/paths.d/npm.sh
+++ .shell/paths.d/npm.sh
@@ -2,7 +2,11 @@
 
 # Customize to your needs...
 # export PATH=$HOME/.local/bin:./node_modules/.bin:$HOME/bin:/usr/lib/lightdm/lightdm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$PATH
-pathprepend $HOME/.local/bin ./node_modules/.bin $HOME/bin /usr/lib/lightdm/lightdm /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games $PATH
+
+
+if [ -d $HOME/.node/bin ]; then
+    pathprepend $HOME/.node/bin 
+fi
 
 if [ -d /usr/local/share/npm/lib/node_modules ]; then
     export NODE_PATH=/usr/local/share/npm/lib/node_modules
