14963f40f65f3647e4a2582e3a34f73fc3fcddaa
Tony Narlock <tony@git-pull.com>
2015-09-26 18:11:40 -0500

    export TTY=$(tty) to env vars for vim tty detect

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -3,3 +3,5 @@
 alias clear_pyc='find . -name "*.pyc" -exec rm -rf {} \;'
 
 alias configure_vim='./configure --with-features=huge --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-luainterp --enable-gui --enable-multibyte'
+
+export TTY=$(tty)
