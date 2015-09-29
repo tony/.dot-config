fc7c9bc18ef6eee00d277db59b653e0c2c30703a
Tony Narlock <tony@git-pull.com>
2015-09-29 14:51:23 -0500

    rbenv: find rbenv installed from bsd port

--- .shell/env.d/rbenv.sh
+++ .shell/env.d/rbenv.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# rbenv
-if [ -d $HOME/.rbenv/bin ]; then
+if [ -f /usr/local/bin/rbenv ]; then # installed via freebsd ports pkg
+    eval "$(rbenv init -)"
+elif [ -d $HOME/.rbenv/bin ]; then
     pathprepend $HOME/.rbenv/bin
     eval "$(rbenv init -)"
 elif [ -f /usr/lib/rbenv/libexec/rbenv ]; then
@@ -11,3 +12,6 @@ elif [ -f /usr/local/opt/rbenv/libexec/rbenv ]; then
     pathprepend /usr/local/opt/rbenv/libexec/
     eval "$(rbenv init -)"
 fi
+
+
+
