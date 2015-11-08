7cd9e13b7b8b1087c3b8f1fec973b028ebb9295f
Tony Narlock <tony@git-pull.com>
2015-11-08 15:46:31 -0600

    check for golang path and add it. check for /Users/tony/work/go

--- .shell/paths.d/golang.sh
+++ .shell/paths.d/golang.sh
@@ -1,5 +1,10 @@
 if [ -d $HOME/gocode ]; then
     export GOPATH=$HOME/gocode
+elif [ -d $HOME/work/go ]; then
+    export GOPATH=$HOME/work/go
+fi
+
+if [ -x $GOPATH ]; then
     if [ -d $GOPATH/bin ]; then
         pathprepend $GOPATH/bin
     fi
