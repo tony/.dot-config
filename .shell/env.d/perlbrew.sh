053f60621f126c4102c3dcefd5d4d5acf2def448
Tony Narlock <tony@git-pull.com>
2015-11-15 07:23:10 -0600

    Fix perlbrew script

--- .shell/env.d/perlbrew.sh
+++ .shell/env.d/perlbrew.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
+#
+# Before installation, set your PERLBREW_ROOT, or else it will default
+# to $HOME/perl5/perlbrew
+#
+# export PERLBREW_ROOT=$HOME/.perl5/perlbrew;
+#
+# FreeBSD:
+#     \fetch -o- http://install.perlbrew.pl | sh
+#
+# Linux:
+#     curl -kL http://install.perlbrew.pl | bash
 
 
-if [ -f $HOME/.perl5/perlbrew ];then
-    export PERLBREW_ROOT=$HOME/.perl5/perlbrew
-    source $PERLBREW_ROOT/etc/bashrc
+if [ -d "$HOME/.perl5/perlbrew" ]; then
+    export PERLBREW_ROOT=${1-$HOME/.perl5/perlbrew}
+    . "$PERLBREW_ROOT/etc/bashrc"
 fi
