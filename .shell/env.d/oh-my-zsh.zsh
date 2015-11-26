282122b6f5227152fd6d6c52bc9270e27d41669c
Tony Narlock <tony@git-pull.com>
2015-11-26 16:17:23 -0600

    update oh-my-zsh setup to use pure

--- .shell/env.d/oh-my-zsh.zsh
+++ .shell/env.d/oh-my-zsh.zsh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
 ## Path to your oh-my-zsh configuration.
+## Note this is used by the install.sh temporary, we don't recommend or understand
+## why they didn't pick a more specific variable.
 ZSH=$HOME/.oh-my-zsh
-ZSH_CUSTOM=$HOME/.config/oh-my-zsh/custom
 
 _prep_zsh() {
     export DISABLE_AUTO_TITLE="true"  # for tmuxp
@@ -12,8 +13,21 @@ _prep_zsh() {
     plugins=(git virtualenv)
     source $ZSH/oh-my-zsh.sh
 }
-if [ -d $ZSH ]; then
+
+# $ZSH could be populated from dotfiles --sync / setup of custom modules causing
+# a race condition.  The custom modules in the dot-config may be symlinked before
+# oh-my-zsh is created.
+if [ -d $ZSH/.git ]; then
     _prep_zsh
 else
+    # ~/.oh-my-zsh/custom may already be populated, we need it out of the way
+    if [ -d $ZSH ]; then
+        mv $ZSH $HOME/.oh-my-zsh.bak
+    fi
+
     curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+    if [ -d $HOME/.dot-config/.oh-my-zsh/custom/ ]; then
+        ln -s $HOME/.dot-config/.oh-my-zsh/custom/* $HOME/.oh-my-zsh/custom/
+    fi
 fi
