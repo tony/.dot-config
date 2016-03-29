a6384568b303d8670c1a88f20e14372c6e16af57
Tony Narlock <tony@git-pull.com>
2016-03-29 03:33:21 -0500

    remove neovim alias

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -4,8 +4,8 @@ alias clear_pyc='find . -name "*.pyc" -exec rm -rf {} \;'
 
 alias configure_vim='./configure --with-features=huge --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-luainterp --enable-gui --enable-multibyte'
 
-if [ -f /usr/local/bin/nvim ]; then
-    alias vim='/usr/local/bin/nvim'
-fi
+# if [ -f /usr/local/bin/nvim ]; then
+#     alias vim='/usr/local/bin/nvim'
+# fi
 
 export TTY=$(tty)
