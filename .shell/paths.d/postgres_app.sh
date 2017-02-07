9694080f94bc763db4e60323763923a50921f383
Tony Narlock <tony@git-pull.com>
2017-02-07 12:59:31 -0600

    add postgres.app 9.6 support

--- .shell/paths.d/postgres_app.sh
+++ .shell/paths.d/postgres_app.sh
@@ -3,3 +3,7 @@
 if [ -d /Applications/Postgres.app/Contents/Versions/9.4/bin ]; then
     pathappend /Applications/Postgres.app/Contents/Versions/9.4/bin
 fi
+
+if [ -d /Applications/Postgres.app/Contents/Versions/9.6/bin ]; then
+    pathappend /Applications/Postgres.app/Contents/Versions/9.6/bin
+fi
