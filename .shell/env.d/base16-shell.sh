aff566b9313791c99fe2a57a5023a50d770ea8fb
Tony Narlock <tony@git-pull.com>
2017-06-28 21:56:56 -0500

    improve base16 256 color loading

--- .shell/env.d/base16-shell.sh
+++ .shell/env.d/base16-shell.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-export BASE16_SCHEME="solarized"
-BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
-[[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
+export BASE16_SCHEME="tomorrow-night"
+BASE16_SHELL="$HOME/.config/base16-shell/"
+BASE16_FILE="$BASE16_SHELL/scripts/base16-$BASE16_SCHEME.sh"
+[[ -s $BASE16_FILE ]] && . $BASE16_FILE
+
+[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
