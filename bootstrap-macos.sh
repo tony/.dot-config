286ca5f3681d065cce875733ffbdd9d713fcff4e
Tony Narlock <tony@git-pull.com>
2017-11-25 19:49:11 -0600

    add macos bootstrap script

---
+++ bootstrap-macos.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+brew install keychain \
+  memcached \
+  golang \
+  caskroom/cask/spotify \
+  tmux \
+  the_silver_searcher \
+  ctags \
+  gdal libgeoip \
+  node \
+  yarn \
+  caskroom/cask/openemu \
+  htop \
+  cmake ninja \
+  caskroom/cask/alfred \
+  tig
