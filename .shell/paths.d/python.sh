3e45b7ef4017e58e6a2a3d063796ebfeec0f86f5
Tony Narlock <tony@git-pull.com>
2018-03-11 21:10:01 -0500

    update mac path for Python paths

--- .shell/paths.d/python.sh
+++ .shell/paths.d/python.sh
@@ -12,7 +12,10 @@ if [ -d "{$HOME}/Library/Python/3.5/bin" ]; then
     pathprepend "$HOME/Library/Python/3.5/bin"
 fi
 
+if [ -d "{$HOME}/Library/Python/3.6/bin" ]; then
+    pathprepend "$HOME/Library/Python/3.6/bin"
+fi
+
 if [ -d "$HOME/.local/bin" ]; then
     pathprepend "$HOME/.local/bin"
 fi
-
