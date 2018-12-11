93f8c52128b12d67ba833882bf6327457f94a394
Tony Narlock <tony@git-pull.com>
2018-12-11 14:43:06 -0600

    Update clear_pyc to clean out __pycache__ (python 3)

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-alias clear_pyc='find . -name "*.pyc" -exec rm -rf {} \;'
+alias clear_pyc='find . | grep -E "(__pycache__|\.pyc|\.pyo$$)" | xargs rm -rf'
 
 alias configure_vim='./configure --with-features=huge --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-luainterp --enable-gui --enable-multibyte'
 
