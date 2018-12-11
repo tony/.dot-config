562ad7417631dbe9c4c43399c2d1575a4e1d59f0
Tony Narlock <tony@git-pull.com>
2018-12-11 14:43:58 -0600

    Clear empty dirs: clear_empty_dirs

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 alias clear_pyc='find . | grep -E "(__pycache__|\.pyc|\.pyo$$)" | xargs rm -rf'
+alias clear_empty_dirs='find . -type d -empty -delete'
 
 alias configure_vim='./configure --with-features=huge --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-luainterp --enable-gui --enable-multibyte'
 
