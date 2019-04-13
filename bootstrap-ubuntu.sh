7685358d5f384bfc31396dcbcdd07e8d189ebbdb
Tony Narlock <tony@git-pull.com>
2019-04-12 19:45:11 -0500

    Remove broken / out of date ubuntu repos

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -22,7 +22,6 @@ fonts-noto-cjk \
 xfonts-wqy
 
 
-sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.7
 
 
@@ -40,14 +39,6 @@ echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sourc
 
 # 4. Install Spotify
 sudo apt-get install spotify-client
-
-
-sudo add-apt-repository ppa:sergio-br2/vbam-trunk
-
-
-sudo apt-get update
-sudo apt-get install vbam
-
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
