54b5c48d4d225a9a21a4f6f31b24b06d063d58b9
Tony Narlock <tony@git-pull.com>
2019-04-16 09:26:59 -0500

    Support ed25519 ssh keys

--- .shell/env.d/keychain.sh
+++ .shell/env.d/keychain.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 if command -v keychain > /dev/null 2>&1; then
-    eval `keychain --quiet --eval ~/.ssh/id_rsa`
+    if [ -f $HOME/.ssh/id_rsa ]; then
+        eval `keychain --quiet --eval ~/.ssh/id_rsa`
+    elif [ -f $HOME/.ssh/id_ed25519 ]; then
+        eval `keychain --quiet --eval ~/.ssh/id_ed25519`
+    fi
 fi
