1185cb5a7af2e65763197a4e8d0a223e1921f329
Tony Narlock <tony@git-pull.com>
2019-07-18 10:43:11 -0500

    SSH keychain fixes
    See also: https://github.com/funtoo/keychain/issues/61#issuecomment-351032984

--- .shell/env.d/keychain.sh
+++ .shell/env.d/keychain.sh
@@ -2,8 +2,8 @@
 
 if command -v keychain > /dev/null 2>&1; then
     if [ -f $HOME/.ssh/id_rsa ]; then
-        eval `keychain --quiet --eval ~/.ssh/id_rsa`
+        eval `keychain --quiet --stop others --inherit any --eval ~/.ssh/id_rsa`
     elif [ -f $HOME/.ssh/id_ed25519 ]; then
-        eval `keychain --quiet --eval ~/.ssh/id_ed25519`
+        eval `keychain --quiet --stop others --inherit any --eval ~/.ssh/id_ed25519`
     fi
 fi
