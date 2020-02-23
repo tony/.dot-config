bfd0a23311a9d8560248dff3c60871b5373f6d1c
Tony Narlock <tony@git-pull.com>
2020-02-23 12:29:31 -0600

    Remove more unused aliases

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -6,8 +6,4 @@ alias clear_empty_dirs='find . -type d -empty -delete'
 # Thank you https://stackoverflow.com/a/1267524
 alias my_internal_ip='python -c "import socket; print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith(\"127.\")][:1], [[(s.connect((\"8.8.8.8\", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])"'
 
-# if [ -f /usr/local/bin/nvim ]; then
-#     alias vim='/usr/local/bin/nvim'
-# fi
-
 export TTY=$(tty)
