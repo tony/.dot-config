a4f9785f431eb34c74c969fa5f4e688a123e0404
Tony Narlock <tony@git-pull.com>
2022-03-15 10:15:53 -0500

    python-breakpoint check strategy for ipython

--- .shell/env.d/python-breakpoint.sh
+++ .shell/env.d/python-breakpoint.sh
@@ -3,3 +3,10 @@
 if command -v ipdb > /dev/null 2>&1 || command -v ipdb3 > /dev/null 2>&1; then
     export PYTHONBREAKPOINT=ipdb.set_trace
 fi
+
+python -c "import ipython" &> /dev/null
+
+if [[ $? -eq 2 ]]; then
+  export PYTHONBREAKPOINT="ipython.set_trace"
+fi
+
