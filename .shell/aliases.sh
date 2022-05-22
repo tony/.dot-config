99acd999b274cca1d9b840e70ae1d5a282d6ce28
Tony Narlock <tony@git-pull.com>
2022-05-22 10:14:59 -0500

    New alias: update_repos

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -7,6 +7,7 @@ alias clear_empty_dirs='find . -type d -empty -delete'
 alias my_internal_ip='python -c "import socket; print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith(\"127.\")][:1], [[(s.connect((\"8.8.8.8\", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])"'
 
 alias update_packages='cd ~/.dot-config; make global_update; cd -'
+alias update_repos='cd ~/.dot-config; make vcspull; cd -'
 
 export TTY=$(tty)
 
