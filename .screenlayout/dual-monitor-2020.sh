fce0faab93054e641c08d0468930f357350e617b
Tony Narlock <tony@git-pull.com>
2022-11-12 17:17:58 -0600

    Remove .screenlayout scripts from arandr

--- .screenlayout/dual-monitor-2020.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-5 --off --output DP-4 --off --output DP-3 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-0 --off
--- .screenlayout/dual-monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 3840x0 --rotate normal --output VGA1 --off
