30b1c919c301bf4c9cec7760fa321ca867fbb58d
Tony Narlock <tony@git-pull.com>
2022-11-13 13:04:37 -0600

    Add alias to bench

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -7,3 +7,5 @@ alias update_packages='pushd ~/.dot-config; make global_update; popd;'
 alias update_repos='pushd ~/.dot-config; make vcspull; popd;'
 
 export TTY=$(tty)
+
+alias bench='for i in $(seq 1 10); do /usr/bin//time /bin/zsh -i -c exit; done;'
