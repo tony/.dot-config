0a22d06b07fcba2fc67a83467fc199993f5aa01d
Tony Narlock <tony@git-pull.com>
2024-01-14 10:33:14 -0600

    fish config: Add emacs and vi bindings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,3 +11,14 @@ if status is-interactive
    set -lx SHELL fish
    keychain --eval --agents ssh --quiet -Q id_ed25519 --nogui | source
 end
+
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
+
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
+end
