9e020c5759420247dd4ba6c70a650a931890c2a5
Tony Narlock <tony@git-pull.com>
2024-02-18 13:21:03 -0600

    fish: fish_indent --write config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,26 +6,26 @@ set -Ux SUDO_EDITOR $EDITOR
 set fish_greeting
 
 if test -d $ASDF_DIR
-  if ! test ~/.config/fish/completions/asdf.fish
-    mkdir -p ~/.config/fish/completions; and ln -s $ASDF_DIR/completions/asdf.fish ~/.config/fish/completions
-  end
-  source $ASDF_DIR/asdf.fish
+    if ! test ~/.config/fish/completions/asdf.fish
+        mkdir -p ~/.config/fish/completions; and ln -s $ASDF_DIR/completions/asdf.fish ~/.config/fish/completions
+    end
+    source $ASDF_DIR/asdf.fish
 else
-  git clone https://github.com/asdf-vm/asdf.git $ASDF_DIR --branch v0.14.0
+    git clone https://github.com/asdf-vm/asdf.git $ASDF_DIR --branch v0.14.0
 end
 
 if ! type -q fisher
-  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 end
 
 if type -q yarn
-  set -U fish_user_paths (yarn global bin) $fish_user_paths
+    set -U fish_user_paths (yarn global bin) $fish_user_paths
 end
 
 if status is-interactive
-   # Commands to run in interactive sessions can go here
-   set -lx SHELL fish
-   keychain --eval --agents ssh --quiet -Q id_ed25519 --nogui | source
+    # Commands to run in interactive sessions can go here
+    set -lx SHELL fish
+    keychain --eval --agents ssh --quiet -Q id_ed25519 --nogui | source
 end
 
 function fish_user_key_bindings
