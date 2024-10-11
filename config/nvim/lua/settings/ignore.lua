59d262a0190edb27ecd4a94da398aa211bc2dd88
Tony Narlock <tony@git-pull.com>
2024-10-11 08:59:00 -0500

    nvim: Use ignore.lua conversion from claude

--- config/nvim/lua/settings/ignore.lua
+++ config/nvim/lua/settings/ignore.lua
@@ -1,53 +1,49 @@
--- lua/settings/ignore.lua
+-- ~/.config/nvim/lua/config/ignore.lua
 
--- NERDTree Ignore Patterns
-vim.g.NERDTreeIgnore = {
-    "\\~$", "\\.pyc", "\\.swp$", "\\.git$", "\\.hg", "\\.svn",
-    "\\.ropeproject", "\\.o", "\\.bzr", "\\.ipynb_checkpoints$",
-    "__pycache__", "\\.egg$", "\\.egg-info$", "\\.tox$", "\\.idea$", "\\.sass-cache",
-    "\\.mypy_cache", "\\.ruff_cache", "\\.pytest_cache", "\\.env$", "\\.env[0-9]$",
-    "\\.venv$", "\\.venv[0-9]$", "\\.coverage$", "\\.tmp$", "\\.gitkeep$", "\\.vscode$",
-    "\\.splinter-screenshots$", "\\.webassets-cache$", "\\.vagrant$", "\\.DS_Store",
-    "\\.env-pypy$", "\\.debug.{d,o}$"
+local M = {}
+
+-- Common ignore patterns
+local common_ignores = {
+  "*.o", "*.obj", "*~", "*.pyc", "*.swp", ".git", ".hg", ".svn",
+  ".ropeproject", ".bzr", ".ipynb_checkpoints", "__pycache__",
+  "*.egg", "*.egg-info", ".tox", ".idea", ".sass-cache", ".mypy_cache",
+  ".ruff_cache", ".pytest_cache", ".env", ".env[0-9]", ".venv", ".venv[0-9]",
+  ".coverage", ".tmp", ".gitkeep", ".vscode", ".splinter-screenshots",
+  ".webassets-cache", ".vagrant", ".DS_Store", ".env-pypy", "*.debug.d",
+  "*.debug.o", "vendor/rails/**", "vendor/cache/**", "*.gem", "log/**",
+  "tmp/**", "*.png", "*.jpg", "*.gif", "*.so", "*.zip", "*/.Trash/**",
+  "*.pdf", "*.dmg", "*/Library/**", "*/.rbenv/**", "*/.nx/**", "*.app"
 }
 
--- vimfiler Ignore Pattern
-vim.g.vimfiler_ignore_pattern = '\\%(\\.ini\\|\\.sys\\|\\.bat\\|\\.BAK\\|\\.DAT\\|\\.pyc\\|\\.egg-info\\)$\\|' ..
-    '^\\%(\\.gitkeep\\|\\.coverage\\|\\.webassets-cache\\|\\.vagrant\\)$\\|' ..
-    '^\\%(\\.env\\|\\.ebextensions\\|\\.elasticbeanstalk\\|Procfile\\)$\\|' ..
-    '^\\%(\\.git\\|\\.tmp\\|__pycache__\\|\\.DS_Store\\|\\.o\\|\\.tox\\|\\.idea\\|\\.ropeproject\\)$'
+-- Set up ignore patterns for various plugins
 
--- Wildignore Patterns for Tab Completion
-vim.opt.wildignore:append {
-    "*.o", "*.obj", "*~", "*.pyc", "*.debug.o", "*.debug.d",
-    ".env", ".env[0-9]+", ".env-pypy", ".venv", ".venv[0-9]+",
-    ".venv-pypy", ".git", ".gitkeep", ".tmp", ".coverage",
-    "*DS_Store*", ".sass-cache/**", ".vscode/", ".*_cache/", "__pycache__/**",
-    ".webassets-cache/", "vendor/rails/**", "vendor/cache/**",
-    "*.gem", "log/**", "tmp/**", ".tox/**", ".idea/**",
-    ".vagrant/**", ".coverage/**", "*.egg", "*.egg-info",
-    "*.png", "*.jpg", "*.gif", "*.so", "*.swp", "*.zip",
-    "*/.Trash/**", "*.pdf", "*.dmg", "*/Library/**", "*/.rbenv/**",
-    "*/.nx/**", "*.app"
-}
+-- Telescope (replaces Unite)
+local telescope_ignores = vim.tbl_extend("force", common_ignores, {
+  ".git/", "node_modules", "bower_components", "%.jpg", "%.jpeg", "%.png",
+  "%.svg", "%.otf", "%.ttf", "%.webp", "%.eot", "%.gif", "%.woff", "%.woff2"
+})
 
--- Netrw Ignore Patterns
-vim.g.netrw_list_hide = [[\.o,\.obj,*\~,\.pyc,\.debug\.d,\.debug\.o,\.env,\.env[0-9].,\.env-pypy,\.venv,\.venv[0-9].,\.venv-pypy,\.git/,\.gitkeep,\.vagrant,\.tmp,\.coverage$,\.DS_Store,__pycache__,\.*_cache/,\.webassets-cache/,\.sass-cache/,\.vscode/,\.splinter-screenshots/,\.ropeproject/,vendor/rails/,vendor/cache/,\.gem,\.ropeproject/,\.coverage/,log/,tmp/,\.tox/,\.idea/,\.egg,\.egg-info,\.png,\.jpg,\.gif,\.so,\.swp,\.zip,/\Trash/,\.pdf,\.dmg,/\Library/,/\.rbenv/,*/\.nx/**,*.app]]
+M.setup = function()
+  -- Neo-tree (replaces NERDTree and Vimfiler)
+  vim.g.neo_tree_remove_legacy_commands = true
 
--- Unite Ignore Patterns (if still used)
-pcall(function()
-  vim.fn['unite#custom#source'](
-    'buffer,file,file_rec/async,file_rec,file_mru,file,grep',
-    'ignore_pattern',
-    table.concat({
-      '\\.DS_Store', '\\.tmp/', '\\.git/', '\\.gitkeep', '\\.hg/', '\\.tox',
-      '\\.idea', '\\.pyc', '\\.png', '\\.gif', '\\.jpg', '\\.svg', '\\.eot',
-      '\\.ttf', '\\.woff', '\\.ico', '\\.o', '__pycache__', '.env', '.env*',
-      '.venv', '.venv*', '.vagrant', '_build', 'dist', '*.tar.gz', '*.zip',
-      'node_modules', 'bower_components', '.*\\.egg', '*.egg-info', '.*egg-info.*',
-      'git5/.*/review/', 'google/obj/', '\\.webassets-cache/', '\\.sass-cache/',
-      '\\.vscode/', '\\.coverage/', '\\.m2/', '\\.activator/', '\\.composer/',
-      '\\.cache/', '\\.npm/', '\\.node-gyp/', '\\.sbt/', '\\.ivy2/', '\\.local/activator/'
-    }, '\\|')
-  )
-end)
+  -- Wildignore (used by Neovim's native file browsing)
+  vim.opt.wildignore = common_ignores
+
+  -- Telescope configuration
+  require("telescope").setup {
+    defaults = {
+      file_ignore_patterns = telescope_ignores
+    }
+  }
+
+  -- Configure other plugins that might need ignore patterns
+  -- For example, if you're using nvim-tree:
+  -- require'nvim-tree'.setup {
+  --   filters = {
+  --     custom = common_ignores
+  --   }
+  -- }
+end
+
+return M
