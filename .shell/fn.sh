c66bd90fdc095df03a2ec8cae2c61412f5a43555
Tony Narlock <tony@git-pull.com>
2024-11-17 18:00:04 -0600

    zsh: Function to rerun

---
+++ .shell/fn.sh
@@ -0,0 +1,27 @@
+rerun() {
+  if [[ $# -lt 2 ]]; then
+    echo "Usage: rerun <n> -- <command>"
+    return 1
+  fi
+
+  local n=$1
+  shift
+
+  if [[ $1 != "--" ]]; then
+    echo "Usage: rerun <n> -- <command>"
+    return 1
+  fi
+  shift
+
+  local cmd=("$@")
+
+  for i in $(seq 1 $n); do
+    echo "Run #$i"
+    "${cmd[@]}"
+    local exit_code=$?
+    if [[ $exit_code -ne 0 ]]; then
+      echo "Command failed on run #$i"
+      break
+    fi
+  done
+}
