67b799dec6d2a5a07f185f6c144d46be0415e7a3
Tony Narlock <tony@git-pull.com>
2025-01-30 16:38:07 -0600

    fzf: Refactor

--- .shell/vars.d/fzf.sh
+++ .shell/vars.d/fzf.sh
@@ -1,32 +1,34 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
-# IGNORE_FILE_EXT is set by ./ignore.sh
-if [ -z "$IGNORE_FILE_EXT" ]; then  # No ignores set
+# Use [[ ... ]] for zsh
+if [[ -z "$IGNORE_FILE_EXT" ]]; then
+    # No ignores
+    FZF_FIND_COMMAND='find . -path "*/.*" -prune -o \( -type f -o -type l \) -print'
+else
+    # Build a grep command that ignores certain extensions/patterns
+    FZF_CUSTOM_GREP_IGNORE="grep -E --ignore-case --invert-match \
+        -e '.*\.($IGNORE_FILE_EXT)' \
+        -e '.*($IGNORE_FILE_WILD).*'"
 
-    export FZF_FIND_COMMAND="find . -path '*/\.*' -prune -o -type f -print -o -type l -print"
-
-    export FZF_DEFAULT_COMMAND="(git ls-files --recurse-submodules & git ls-files --exclude-standard --others ||
-        ${FZF_FIND_COMMAND} | \
-            sed s/^..// \
-        ) 2> /dev/null"
-
-    export FZF_DEFAULT_COMMAND='(git ls-files --recurse-submodules ||
-        find . -path "*/\.*" -prune -o -type f -print -o -type l -print |
-    sed s/^..//) 2> /dev/null'
-
-else  # Exists
-    export FZF_CUSTOM_GREP_IGNORE="grep --ignore-case --invert-match -e '.*[.]\(\
-        ${IGNORE_FILE_EXT} \
-        \)' -e '.*\(${IGNORE_FILE_WILD}\).*'
-    "
-
-    export FZF_FIND_COMMAND="find . -path '*/\.*' -prune -o -type f -print -o -type l \
-    \( -iname '.*\($IGNORE_FILE_WILD\).*' -o -iname '.*[.]\($IGNORE_FILE_EXT\)' \) -print"
-
-    export FZF_DEFAULT_COMMAND="(git ls-files --recurse-submodules & git ls-files --exclude-standard --others ||
-        ${FZF_FIND_COMMAND} | \
-            sed s/^..// \
-        ) | ${FZF_CUSTOM_GREP_IGNORE} 2> /dev/null"
-
-    export FZF_CTRL_T_COMMAND="$FZF_FIND_COMMAND | ${FZF_CUSTOM_GREP_IGNORE} 2> /dev/null"
+    FZF_FIND_COMMAND="find . -path '*/.*' -prune -o \( -type f -o -type l \) -print \
+        | $FZF_CUSTOM_GREP_IGNORE"
 fi
+
+# Remove leading './' from find output
+# Could do sed 's|^\./||'
+# or cut -c3- if you trust lines always start with './'
+STRIP_CMD="sed 's|^\./||'"
+
+# If git ls-files fails, fallback to raw find
+# Use && or ; to run them in sequence
+FZF_DEFAULT_COMMAND="( \
+    (git ls-files --recurse-submodules; \
+    git ls-files --exclude-standard --others) 2>/dev/null || \
+    $FZF_FIND_COMMAND \
+  ) | $STRIP_CMD 2>/dev/null"
+
+export FZF_FIND_COMMAND
+export FZF_DEFAULT_COMMAND
+
+# If you want the Ctrl+T command to also do the ignoring:
+export FZF_CTRL_T_COMMAND="$FZF_FIND_COMMAND | $STRIP_CMD 2>/dev/null"
