85b5b8911f4616c26ca4bf3c856023a7c4cbe42a
Tony Narlock <tony@git-pull.com>
2025-02-22 13:39:55 -0600

    feat(fish): configure key bindings - Port Zsh key bindings to Fish, including vi mode, FZF integration, and command editing

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,21 @@
 function fish_user_key_bindings
-  fzf --fish | source
+    # Use emacs key bindings
+    fish_default_key_bindings -M insert
+
+    # Add vi key bindings on top
+    fish_vi_key_bindings --no-erase insert
+
+    # FZF key bindings if available
+    if type -q fzf_key_bindings
+        fzf_key_bindings
+    end
+
+    # Edit command in external editor (like Zsh's edit-command-line)
+    bind --user \cx\ce edit_command_buffer
+    bind --user -M insert \cx\ce edit_command_buffer
+    bind --user -M visual \cx\ce edit_command_buffer
+
+    # History substring search bindings
+    bind --user -M insert \e[A history-prefix-search-backward
+    bind --user -M insert \e[B history-prefix-search-forward
 end
