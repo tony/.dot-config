e0c18d04a2745c7a727f875f178ac7462c19b292
Tony Narlock <tony@git-pull.com>
2025-12-28 11:26:49 -0600

    fish: remove duplicate poetry activation (saves ~400ms startup)
    fish_venv.fish had duplicate poetry shell activation code that was
    already handled by fish-poetry.fish. The duplicate used the deprecated
    `poetry shell` command, adding ~880ms to every fish startup.
    
    Removed the poetry section from fish_venv.fish since fish-poetry.fish
    already handles poetry projects with the Poetry 2.0 compatible approach.
    
    Before: Fish 1.5s, Zsh 0.94s (Zsh 37% faster)
    After:  Fish 1.1s, Zsh 1.33s (Fish 17% faster)

--- config/fish/conf.d/fish_venv.fish
+++ config/fish/conf.d/fish_venv.fish
@@ -1,62 +1,6 @@
-# https://github.com/ryoppippi/fish-poetry/blob/main/conf.d/fish-poetry.fish
-# license: MIT (2024-02-04, 2e5369f)
+# Auto-activate Python virtual environments on directory change
+# Poetry projects are handled by conf.d/fish-poetry.fish
 #
-# https://gist.github.com/tommyip/cf9099fa6053e30247e5d0318de2fb9e
-# originally from https://gist.github.com/tommyip/cf9099fa6053e30247e5d0318de2fb9e
-
-if command -s poetry > /dev/null
-
-    # complete --command poetry --arguments "(env _POETRY_COMPLETE=complete-fish COMMANDLINE=(commandline -cp) poetry)" -f
-
-    # function that loads environment variables from a file
-    function posix-source
-        for i in (cat $argv)
-            if test (echo $i | sed -E 's/^[[:space:]]*(.).+$/\\1/g') != "#" && test -n $i
-                set arr (string split -m1 = $i)
-                set -gx $arr[1] $arr[2]
-            end
-        end
-    end
-
-
-    function __poetry_shell_activate --on-variable PWD
-        if status --is-command-substitution
-            return
-        end
-        if not test -e "$PWD/pyproject.toml"
-            if not string match -q "$__poetry_fish_initial_pwd"/'*' "$PWD/"
-                set -U __poetry_fish_final_pwd "$PWD"
-                exit
-            end
-            return
-        end
-
-        if not test -n "$POETRY_ACTIVE"
-          if poetry env info -p >/dev/null 2>&1
-            set -x __poetry_fish_initial_pwd "$PWD"
-            if test "$FISH_POETRY_LOAD_ENV" -a -e "$PWD/.env"
-                echo "Setting environment variables..."
-                posix-source $PWD/.env
-            end
-
-            poetry shell --quiet --no-ansi
-
-            set -e __poetry_fish_initial_pwd
-            if test -n "$__poetry_fish_final_pwd"
-                cd "$__poetry_fish_final_pwd"
-                set -e __poetry_fish_final_pwd
-            end
-          end
-        end
-    end
-    __poetry_shell_activate
-else
-    function poetry -d "https://python-poetry.org"
-        echo "Install https://python-poetry.org to use this plugin." > /dev/stderr
-        return 1
-    end
-end
-
 # Based on https://gist.github.com/tommyip/cf9099fa6053e30247e5d0318de2fb9e
 # Based on https://gist.github.com/bastibe/c0950e463ffdfdfada7adf149ae77c6f
 # Based on https://github.com/nakulj/auto-venv/
